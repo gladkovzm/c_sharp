d5b971bd44fb2dfe24f35ccd17cba856b8b7514f
Gladkov Ilya <gladkovzm@gmail.com>
2022-08-04 21:29:11 +0300

    5

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.Clear();
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int sqrt = number * number;
+// number *= number
